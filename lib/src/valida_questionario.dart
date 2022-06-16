768ea699597c67263fdb22eeee117cfe9a7a1a62
Felipe Benincaza <fbenincaza@gmail.com>
2022-06-16 12:51:19 -0300

    - adição do valida quesito7 e teste

--- lib/src/valida_questionario.dart
+++ lib/src/valida_questionario.dart
@@ -1,13 +1,14 @@
 library infpack;
 
-/// A Calculator.
+/// Valida regras de salvamento do questionário .
 class ValidaQuestionario {
 
-  //verifica se a localização foi capturada.
+  ///verifica se a localização foi capturada.
   String verificaGps(String latitude, String longitude) => latitude.isEmpty && longitude.isEmpty ?
-      'Não foi capturada a localização da abertura do questionário. Confirmar salvar sem capturar?' :
+      'Não foi capturada a localização da abertura do questionário. Confirma salvar sem capturar?' :
       '';
 
+  ///Verifica se todos os quesitos obrigatórios foram respondidos.
   String verificaQuesitosRespondidos(String quesito1, String quesito2, String quesito3, String quesito4, String quesito5, String quesito6) {
     if (quesito1 == 'Selecione')
       return 'Favor preencher o quesito 1.1.';
@@ -26,6 +27,14 @@ class ValidaQuestionario {
 
     if (quesito6 == 'Selecione')
       return 'Favor preencher o quesito 1.6.';
+
+    return '';
+  }
+
+  /// Verifica se a pergunta 1.6.1 entra no caso de obrigatoriedade para salvar o questionário.
+  String verificaQuesito7(String quesito6, String quesito7) {
+    if (quesito6 == 'Não' && quesito7 == "Selecione")
+      return 'Favor preencher o quesito 1.6.1.';
 
     return '';
   }
--- test/pesquisapack_test.dart
+++ test/pesquisapack_test.dart
@@ -3,10 +3,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pesquisapack/pesquisapack.dart';
 
 void main() {
-  /*test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });*/
+  test('adds one to input values', () {
+    final validaQestionario = ValidaQuestionario();
+
+    expect(validaQestionario.verificaGps('', ''), 'Não foi capturada a localização da abertura do questionário. Confirmar salvar sem capturar?');
+    expect(validaQestionario.verificaGps('-22.912758', '-43.227030'), '');
+
+    String sim = 'Sim';
+    expect(validaQestionario.verificaQuesitosRespondidos(sim, sim, sim, sim, sim, sim), '');
+    expect(validaQestionario.verificaQuesitosRespondidos(sim, sim, 'Selecione', sim, sim, sim), 'Favor preencher o quesito 1.3.');
+    expect(validaQestionario.verificaQuesitosRespondidos('Selecione', sim, 'Selecione', sim, sim, sim), 'Favor preencher o quesito 1.1.');
+    expect(validaQestionario.verificaQuesitosRespondidos(sim, sim, sim, sim, 'Selecione', sim), 'Favor preencher o quesito 1.5.');
+
+    expect(validaQestionario.verificaQuesito7('Não', 'Selecione'), 'Favor preencher o quesito 1.6.1.');
+    expect(validaQestionario.verificaQuesito7('Sim', 'Selecione'), '');
+    expect(validaQestionario.verificaQuesito7('Não', 'Outro destino'), '');
+  });
 }
